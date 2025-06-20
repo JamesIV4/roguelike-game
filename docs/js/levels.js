50530b05d190a09f30745bbe5477c5fde0c54b84
James Pound <james.pound@genesys.com>
2025-06-20 11:08:09 -0400

    Ensure selection is disabled across the game

--- docs/js/levels.js
+++ docs/js/levels.js
@@ -1,4 +1,4 @@
-// Generated: Friday, June 20, 2025 at 10:39:38 AM EDT
+// Generated: Friday, June 20, 2025 at 11:07:30 AM EDT
 export const levelData = [
     `.,.,.,.,.,.,#,#,#,#,#,#,#,#,#,#,#,.,.,.
 .,.,.,.,.,.,#,,,,,,,,,,#,.,.,.
